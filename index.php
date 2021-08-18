80161b8f2c8dccd7b337e59a6e811720e7f4c24c
fredatabo <fredatabo@gmail.com>
2021-08-18 06:26:11 -0700

    a program to print names

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+A program to print out my name
+
+*/
+
+// a function to print name
+
+function printName($name){
+
+    echo "My name is ".$name;
+}
+
+// codes to call the function
+
+printName("Frederick Atabo");
+
+
+?>
